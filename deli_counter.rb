3101dca87b5182ae4cf3fe4d69e4379b21e479ed
Matteo Muscas <thatman.j@gmail.com>
2018-11-19 09:50:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 katz_deli = []
 
 def line(kats_deli)
-  kats_deli.each_with_index{|num, i| j = i + 1;
-    puts "The line is currently: #{num}. #{j}"
+  kats_deli.each_with_index{|num, i|
+    puts "The line is currently: #{num}. #{i+1}"
   end
 
 end
