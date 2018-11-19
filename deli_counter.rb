05825d77178f0bf5c9ac484507bfc3f2398bd563
Matteo Muscas <thatman.j@gmail.com>
2018-11-19 09:40:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ katz_deli = []
 def line(kats_deli)
   kats_deli.each_with_index do |num, index|
     puts "The line is currently: #{kats_deli.length+1}. #{kats_deli[i]}"
-    if (index == 0)
+  elsif (index == 0)
     puts "The line is currenty empty"
   end
-end
-
+end    
+        
   
